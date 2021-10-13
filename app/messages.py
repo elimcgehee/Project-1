6a3b2328e194b62022aa831d767772096e1fa7a0
Eli <elimcgehee2001@gmail.com>
2021-10-12 21:56:30 -0400

    final

---
+++ app/messages.py
@@ -0,0 +1,35 @@
+SPACE = " "
+
+beepboop = [ "📺", "🕹️", "🤖", "👾", "🎮" ]
+online = "- Bot Online 🟢"
+offline = "- Bot Offline 🔴"
+
+startemojis = [ 
+       "🤯",
+       "🥶", 
+       "👾", 
+       "👽", 
+       "👹", 
+       "💀", 
+       "😎", 
+       "😖" 
+       ]
+
+JeffBezos = "@JeffBezos - Howdy Jeffery! Hope you had fun flying to space! Maybe while you're up there, you could also pay some federal income taxes! #JeffBezos"
+ElonMusk = "@elonmusk - Greetings, Elon! I know you're busy with your cars and tunnels, but maybe you could also add paying federal income taxes to the agenda? Cheers! <3 #ElonMusk"
+MarkZuckerberg = "Mark Zuckerberg, I know you're busy allowing Facebook to rapidy spread misinformation, but It would be amazing if you could also pay more taxes! #MarkZuckerberg"
+WarrenBuffet = "Warren Buffet! How's the whole Dairy Queen thing going for you? Just kidding - I don't really care. Pay more taxes though! #warrenbuffett"
+LarryEllison = "@larryellison - Salutations! How is your new $80 million dollar mansion in south Florida doing? Haha just kidding, I couldn't care less... Pay your taxes!"
+
+articles = [ 
+"https://truthout.org/articles/new-analysis-shows-just-how-little-richest-001-percent-pay-in-taxes/",
+"https://www.bbc.com/news/business-57383869",
+"https://www.nytimes.com/2021/06/08/us/politics/income-taxes-bezos-musk-buffett.html",
+"https://thehill.com/policy/finance/557339-some-billionaires-had-years-where-they-paid-no-taxes-report" 
+]
+
+phrase_list = [ "Eli bot initated."
+, "Injecting Eli AI... Eli AI successfully injected!"
+, "Beep Boop", "I'm a bot."
+, "Eli brain initating... Eli brain successfully initiated!"
+, "I'm tired and doing this at 2am..." ]
